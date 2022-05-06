11bc9c6ff8adf09be783694a25f8149e11a73443
Dane Williamson <danezellwilliamson@gmail.com>
2022-05-06 14:39:48 -0500

    sign up form

--- js/script.js
+++ js/script.js
@@ -3,11 +3,18 @@ const navbarLinks = document.getElementsByClassName ('links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
+});
 
 navbarLinks.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-})
+});
+
+function submitButton(){
+  window.dialog.close();
+  alert("Thanks for signing up! We'll be in touch shortly.");
+}
+// dialog.show();
+// dialog.hide();
 //                    sticky nav bar idea
 //  window.addEventListener("scroll", () => {
 //    var header = document.querySelector("header");
